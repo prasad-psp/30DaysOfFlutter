5329c0e1895c1d12bce3c94aa9ab1e84fdae2c3d
Admin <prasad.parshram123@gmail.com>
2022-04-04 13:55:14 +0530

    move to pages package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:catlog_app/login_page.dart';
+import 'package:catlog_app/pages/home_page.dart';
+import 'package:catlog_app/pages/login_page.dart';
 import 'package:flutter/material.dart';
-import 'home_page.dart';
 
 void main() {
   runApp(
