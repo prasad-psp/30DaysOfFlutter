b3b1e562f6029dedfb7347520299364b720abddb
Admin <prasad.parshram123@gmail.com>
2022-04-06 11:14:45 +0530

    SingleChildScrollView, Navigator

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:catlog_app/routes/myroutes.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -8,50 +9,53 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
-        child: Column(
-          children: <Widget>[
-            Image.asset(
-              "assets/images/login_img.png",
-              fit: BoxFit.cover,
-            ),
-            const SizedBox(
-              height: 15,
-            ),
-            Text("Welcome",
-                style: TextStyle(
-                    color: Colors.black,
-                    fontFamily: GoogleFonts.lato().fontFamily,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20)),
-            const SizedBox(
-              height: 15,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: const InputDecoration(
-                        hintText: "Enter username", labelText: "Username"),
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                        hintText: "Enter password", labelText: "Password"),
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  ElevatedButton(
-                      onPressed: () {
-                        print('pressed button');
-                      },
-                      child: const Text("Login"),
-                      style: TextButton.styleFrom())
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Image.asset(
+                "assets/images/login_img.png",
+                fit: BoxFit.cover,
               ),
-            )
-          ],
+              const SizedBox(
+                height: 10,
+              ),
+              Text("Welcome",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: GoogleFonts.lato().fontFamily,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20)),
+              const SizedBox(
+                height: 15,
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          hintText: "Enter username", labelText: "Username"),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                          hintText: "Enter password", labelText: "Password"),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, MyRoutes.homeRoute);
+                        },
+                        child: const Text("Login"),
+                        style: TextButton.styleFrom())
+                  ],
+                ),
+              )
+            ],
+          ),
         ));
   }
 }
