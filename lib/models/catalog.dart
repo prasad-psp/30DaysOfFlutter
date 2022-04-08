f29bd7c489992e368f4376df5379b71109bf579a
Admin <prasad.parshram123@gmail.com>
2022-04-08 11:54:03 +0530

    day 10 : Model class

---
+++ lib/models/catalog.dart
@@ -0,0 +1,9 @@
+class Items {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String imageUrl;
+
+  Items({this.id="", this.name="", this.desc="", this.price=0, this.imageUrl=""});
+}
