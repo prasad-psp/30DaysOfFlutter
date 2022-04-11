6b4dae3f3b89208a91a7b90f98cba859f4271a46
Admin <prasad.parshram123@gmail.com>
2022-04-11 12:33:15 +0530

    day 12 : ListView

---
+++ lib/widget/item_widget.dart
@@ -0,0 +1,31 @@
+import 'package:catlog_app/models/catalog.dart';
+import 'package:flutter/material.dart';
+
+class ItemWidget extends StatelessWidget {
+  final Item item;
+
+  const ItemWidget({Key? key, required this.item}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Card(
+        child: ListTile(
+          onTap: () {
+            print("\$${item.name} pressed");
+          },
+          leading: Image.network(item.imageUrl),
+          title: Text(item.name),
+          subtitle: Text(item.desc),
+          trailing: Text(
+            "\$${item.price.toString()}",
+            textScaleFactor: 1.5,
+            style: const TextStyle(
+                color: Colors.deepPurple, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+    );
+  }
+}
