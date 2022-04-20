ad8a3048a3ebba17785bf852d2e5c80f87813ec1
Admin <prasad.parshram123@gmail.com>
2022-04-20 13:35:45 +0530

    day 22 : Placeholder

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -12,6 +12,33 @@ class CartPage extends StatelessWidget {
         backgroundColor: Colors.transparent,
         title: "Cart".text.make(),
       ),
+      body: Column(children: [
+        const Placeholder().p32().expand(),
+        const Divider(),
+        const _CartTotal(),
+      ]),
+    );
+  }
+}
+
+class _CartTotal extends StatelessWidget {
+  const _CartTotal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 100,
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+        "\$9999".text.xl4.color(context.accentColor).make(),
+        30.widthBox,
+        ElevatedButton(
+          onPressed: () {},
+          child: "Buy".text.make(),
+          style: ButtonStyle(
+              backgroundColor:
+                  MaterialStateProperty.all(context.theme.buttonColor)),
+        ).w32(context)
+      ]),
     );
   }
 }
