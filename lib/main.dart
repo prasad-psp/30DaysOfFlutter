1659ba5b40dc3e7a3fec805178150f88c320b678
Admin <prasad.parshram123@gmail.com>
2022-04-25 16:55:22 +0530

    day 30: Url Strategy

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,16 @@ import 'package:catlog_app/store/store.dart';
 import 'package:catlog_app/widget/mythemes.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
-void main() => runApp(VxState(store: MyStore(), child: MyApp()));
+void main() {
+  appConfig();
+  runApp(VxState(store: MyStore(), child: MyApp()));
+}
+
+void appConfig() {
+  setUrlStrategy(PathUrlStrategy());
+}
 
 class MyApp extends StatelessWidget {
   @override
